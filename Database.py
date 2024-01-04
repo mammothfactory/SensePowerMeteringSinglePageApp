1d077314963d4603448907b14b1ab2b65cf0c8cc
Blaze Sanders <blazes@mfc.us>
2024-01-03 22:54:28 -0600

    WIP: Started database inserts base on queries

--- Database.py
+++ Database.py
@@ -20,6 +20,8 @@ from datetime import datetime, time, timedelta 	# Manipulate calendar dates & ti
 from time import sleep                          # Pause program execution
 import os                                       # Get filename information like directoty and file path
 import csv                                      # Manipulate .CSV files for data reporting
+import json                                     # Use to serialize a list of list and insert into TEXT column of SQLite database
+from typing import Optional
 
 # 3rd Party Libraries
 import pytz 					                # World Timezone Definitions  https://pypi.org/project/pytz/
@@ -49,8 +51,8 @@ class Database:
         self.pollingRate = pollingRate     # In units of Hz
 
         # Create six tables in EnergyReport.db for collecting energy data
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS DailyEnergyTable   (id INTEGER PRIMARY KEY, instantWattage INTEGER, currentCostPerWh INTEGER, timestamp TEXT)''')
-        self.cursor.execute('''CREATE TABLE IF NOT EXISTS WeeklyEnergyTable  (id INTEGER PRIMARY KEY, totalDailyWattHours INTEGER, currentCostPerWh INTEGER, timestamp TEXT)''')
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS DailyEnergyTable   (id INTEGER PRIMARY KEY, totalDailyWattHours INTEGER, currentCostPerWh INTEGER, timestamp TEXT)''')
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS WeeklyEnergyTable  (id INTEGER PRIMARY KEY, totalWeeklyWattHours INTEGER, currentCostPerWh INTEGER, timestamp TEXT)''')
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS MonthlyEnergyTable (id INTEGER PRIMARY KEY, totalWeeklyWattHours INTEGER, currentCostPerWh INTEGER, timestamp TEXT)''')
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS WeekGraphTable     (id INTEGER PRIMARY KEY, wattHours INTEGER, weekNumber TEXT)''')
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS MonthGraphTable    (id INTEGER PRIMARY KEY, wattHours INTEGER, monthNumber TEXT)''')
@@ -73,26 +75,6 @@ class Database:
         # Each year has 12 months
         for monthNumber in range(1, 12):
             self.insert_month_graph_table(0, monthNumber)
-
-
-    def example_tables(self):
-        """ As define by a 1/60 Hz polling rate of DailyEnergyTable, once every 24 hours polling rate for WeeklyEnergyTable, and once per week polling rate for MonthlyEnergyTable
-        """
-        self.insert_daily_energy_table(200, 0.11, "2024-01-01T00:00:00")
-        self.insert_daily_energy_table(220, 0.11, "2024-01-01T23:59:00")
-
-        self.insert_weekly_energy_table(4800, 0.11, "2024-01-01T12:00:00")
-        self.insert_weekly_energy_table(5280, 0.11, "2024-01-02T12:00:00")
-        self.insert_weekly_energy_table(4800, 0.11, "2024-01-03T12:00:00")
-        self.insert_weekly_energy_table(5280, 0.11, "2024-01-04T12:00:00")
-        self.insert_weekly_energy_table(4800, 0.11, "2024-01-05T12:00:00")
-        self.insert_weekly_energy_table(5280, 0.11, "2024-01-06T12:00:00")
-        self.insert_weekly_energy_table(4800, 0.11, "2024-01-07T12:00:00")
-
-        self.insert_monthly_energy_table(33360, 0.11, "2024-01-07T23:59:00")
-        self.insert_monthly_energy_table(36960, 0.11, "2024-01-14T23:59:00")
-        self.insert_monthly_energy_table(33360, 0.11, "2024-01-21T23:59:00")
-        self.insert_monthly_energy_table(36960, 0.11, "2024-01-28T23:59:00")
 
 
     def update_graph_table(self, startDate: str, timeFrame: str = WEEKLY):
@@ -151,26 +133,54 @@ class Database:
         return now
 
 
-    def query_table(self, tableName: str):
-        """ Return every row of a table from a *.db database
+    def example_tables(self):
+        """ As define by a 1/60 Hz polling rate of DailyEnergyTable, once every 24 hours polling rate for WeeklyEnergyTable, and once per week polling rate for MonthlyEnergyTable
+        """
+        self.insert_daily_energy_table(200, 0.11, "2024-01-01T00:00:00")
+        self.insert_daily_energy_table(220, 0.11, "2024-01-01T23:59:00")
+
+        self.insert_weekly_energy_table(4800, 0.11, "2024-01-01T12:00:00")
+        self.insert_weekly_energy_table(5280, 0.11, "2024-01-02T12:00:00")
+        self.insert_weekly_energy_table(4800, 0.11, "2024-01-03T12:00:00")
+        self.insert_weekly_energy_table(5280, 0.11, "2024-01-04T12:00:00")
+        self.insert_weekly_energy_table(4800, 0.11, "2024-01-05T12:00:00")
+        self.insert_weekly_energy_table(5280, 0.11, "2024-01-06T12:00:00")
+        self.insert_weekly_energy_table(4800, 0.11, "2024-01-07T12:00:00")
+
+        self.insert_monthly_energy_table(33360, 0.11, "2024-01-07T23:59:00")
+        self.insert_monthly_energy_table(36960, 0.11, "2024-01-14T23:59:00")
+        self.insert_monthly_energy_table(33360, 0.11, "2024-01-21T23:59:00")
+        self.insert_monthly_energy_table(36960, 0.11, "2024-01-28T23:59:00")
+
+
+    def insert_daily_energy_table(self, energy: int, cost: float, date: str) -> int:
+        """ Insert total daily energy used (in Wh), energy cost (in US Dollars), and DATE of database operation into the DailyEnergyTable if DATE is unqiue, otherwise update total daily energy used
 
         Args:
-            tableName (String): Name of table in database to query
-
-        Returns:
-            List: Tuples from a table, where each row in table is a tuple length n
+            energy (int): ??? (e.g. 2400 Wh)
+            cost (str): ???? (e.g. $0.11)
+            datetime (str): ???? (e.g. 2024-01-01)
         """
-        sqlStatement = f"SELECT * FROM {tableName}"
-        self.cursor.execute(sqlStatement)
+        #CREATE TABLE IF NOT EXISTS DailyEnergyTable   (id INTEGER PRIMARY KEY, totalDailyWattHours INTEGER, currentCostPerWh INTEGER, currentCostPerWh TEXT)
+        # TODO If datetime is already in a row UPDATE row with lastest totalDailyWattHours OTHERWISE INSERT new row 
+        results = self.query_table("DailyEnergyTable", date)
+        
+        if len(results) > 0:
+            idPrimaryKeyToUpdate = results[0][0]
+            self.cursor.execute("UPDATE DailyEnergyTable SET totalDailyWattHours = ?, currentCostPerWh = ?, timestamp = ? WHERE id = ?", (id, energy, cost, datetime))
+        else:
+            self.cursor.execute("INSERT INTO UsersTable (employeeId, firstName, lastName) VALUES (?, ?, ?)", (id, first, last))
 
-        result = self.cursor.fetchall()
-
-        return result
-
-    def insert_daily_energy_table(self, energy: int, cost: float, datetime: str) -> int:
-        databaseIndexInserted = -1 #TODO
-
-        return databaseIndexInserted
+        self.commit_changes()
+        
+        
+        self.cursor.execute("UPDATE DailyEnergyTable SET totalDailyWattHours = ?, currentCostPerWh = ?, timestamp = ? WHERE id = ?", (id, energy, cost, datetime))
+        
+        self.cursor.execute("INSERT INTO DailyEnergyTable (totalDailyWattHours, currentCostPerWh, timestamp) VALUES (?, ?, ?)", (energy, cost, datetime))
+        lastDatabaseIndexInserted = self.cursor.lastrowid
+        self.commit_changes()   
+        
+        return lastDatabaseIndexInserted
 
 
     def insert_weekly_energy_table(self, energy: int, cost: float, datetime: str) -> int:
@@ -239,41 +249,6 @@ class Database:
                 return '',''
 
 
-    def insert_check_out_table(self, id: int) -> tuple:
-        """ Insert date and time (to current mintue) into CheckOutTable of database
-            https://en.wikipedia.org/wiki/ISO_8601
-
-        Args:
-            id (int): Employee ID (from 1 to 9999) linked to internal email (e.g. 9000@mammothfactory.co)
-        """
-        data = self.query_table("CheckOutTable")
-        result = list(filter(lambda t: t[GC.EMPLOYEE_ID_COLUMN_NUMBER] == id, data))
-        if GC.DEBUG_STATEMENTS_ON:  print(f'EMPLOYEE ID FILTER: {result}')
-
-        isoString = '?'
-        currentDateTime = self.get_date_time().isoformat(timespec="minutes")
-
-        try:
-            storedIsoString = result[0][GC.TIMESTAMP_COLUMN_NUMBER]
-            if GC.DEBUG_STATEMENTS_ON:  print(f'ISO DateTime: {storedIsoString}')
-
-        except IndexError:
-            if GC.DEBUG_STATEMENTS_ON: print(f'INSERTING {id} since this employee ID has NOT clocked OUT TODAY')
-            self.cursor.execute("INSERT INTO CheckOutTable (employeeId, timestamp) VALUES (?, ?)", (id, currentDateTime))
-            self.commit_changes()
-            return '',''
-
-        finally:
-            nameResults = []
-            nameResult = self.search_users_table(str(id))
-            if len(nameResults) > 0:
-                englishError = f'{nameResults[0][GC.FIRST_NAME_COLUMN_NUMBER]} {nameResults[0][GC.LAST_NAME_COLUMN_NUMBER]}  you already clocked out today'
-                spanishError = f'{nameResults[0][GC.FIRST_NAME_COLUMN_NUMBER]} {nameResults[0][GC.LAST_NAME_COLUMN_NUMBER]}  ya saliste hoy'
-                return englishError, spanishError
-            else:
-                return '',''
-
-
     def insert_debug_logging_table(self, debugText: str):
         """ Insert debugging text in database for later review
 
@@ -284,98 +259,49 @@ class Database:
         self.commit_changes()
 
 
-    def search_users_table(self, searchTerm: str):
-        """ Search UsersTable table for every occurrence of a string
+    def query_table(self, tableName: str, searchTerm: str, row: Optional[int]= None, column: Optional[int]= None) -> tuple:
+        """ Return every row of a table from a *.db database
 
         Args:
-            searchTerm (str): _description_
+            tableName (String): Name of table in database to query
+            row (Interger): Optional row from tuple to return
+            column (Interger): Optional column from tuple to return
 
         Returns:
-            List: Of Tuples from a UsersTable, where each List item is a row in the table containing the exact search term
+            result: A list of tuples from a table, where each row in table is a tuple of length n
+            isEmpty: Returns True if table is empty, and False otherwise
+            isValid: Returns True is table name exists in EnergyReport.db, and False otherwise
+            
         """
-        self.cursor.execute("SELECT * FROM UsersTable WHERE employeeId LIKE ?", ('%' + str(searchTerm) + '%',))
-        results = self.cursor.fetchall()
-
-        return results
-
-
-    # NO CURRENTLY USED!!! See ManualTImeCalculations.py to see now .csv report are generated
-    def insert_weekly_report_table(self, id: int, date: datetime):
-        """ Build a table using the following rules:
-        
-
-        Args:
-            id (int): Employee ID
-            dateToCalulate (datetime): _description_
-        """
-        
         try:
-            # Get the day of the week (0=Monday, 1=Tuesday, ..., 6=Sunday)
-            dayOfWeek = date.weekday()
-            if Database.DEBUGGING: 
-                dayOfWeek = GC.SUNDAY
-                #currentTime = time(22, 45, 0)
-                #currentTime = time(23, 22, 0)
-                #currentTime = time(23, 59, 0)
-                #currentTime = time(24, 0, 0)
-                #currentTime = time(1, 2, 0)
-                #currentTime = time(2, 59, 0)
-                #currentTime = time(3, 11, 0)
-            
-            dailyHours = self.calculate_time_delta(id, date) 
-            
-            results = self.search_users_table(id)
-            if len(results) > 0:
-                idPrimaryKeyToUpdate = results[0][0]
-                
-                if dayOfWeek == GC.SUNDAY: 
-                    self.cursor.execute("UPDATE WeeklyReportTable SET day6 = ? WHERE id = ?", (dailyHours, idPrimaryKeyToUpdate))
-                elif dayOfWeek == GC.MONDAY:
-                    self.cursor.execute("UPDATE WeeklyReportTable SET day0 = ? WHERE id = ?", (dailyHours, idPrimaryKeyToUpdate))
-                elif dayOfWeek == GC.TUESDAY:
-                    self.cursor.execute("UPDATE WeeklyReportTable SET day1 = ? WHERE id = ?", (dailyHours, idPrimaryKeyToUpdate))
-                elif dayOfWeek == GC.WEDNESDAY:
-                    self.cursor.execute("UPDATE WeeklyReportTable SET day2 = ? WHERE id = ?", (dailyHours, idPrimaryKeyToUpdate))
-                elif dayOfWeek == GC.THURSDAY:
-                    self.cursor.execute("UPDATE WeeklyReportTable SET day3 = ? WHERE id = ?", (dailyHours, idPrimaryKeyToUpdate))
-                elif dayOfWeek == GC.FRIDAY:
-                    self.cursor.execute("UPDATE WeeklyReportTable SET day4 = ? WHERE id = ?", (dailyHours, idPrimaryKeyToUpdate))
-                elif dayOfWeek == GC.SATURDAY:
-                    self.cursor.execute("UPDATE WeeklyReportTable SET day5 = ? WHERE id = ?", (dailyHours, idPrimaryKeyToUpdate))
+            sqlStatement = f"SELECT * FROM {tableName} WHERE content LIKE ?, ('%' + {str(searchTerm)} + '%',)"       
+            #self.cursor.execute("SELECT * FROM DatasetTable WHERE content LIKE ?", ('%' + str(searchTerm) + '%',))
+            self.cursor.execute(sqlStatement)
 
-                data = self.query_table("WeeklyReportTable")
-                if data ==  None:
-                    pass
-                else:
-                    totalHours = sum(data[idPrimaryKeyToUpdate-1][GC.SUNDAY_COLUMN_NUMBERS:GC.SATURDAY_COLUMN_NUMBERS+1])
-                    xComments = data[idPrimaryKeyToUpdate-1][GC.IN_COMMENTS_COLUMN_NUMBERS:GC.OUT_COMMENTS_COLUMN_NUMBERS+1]
-                    self.cursor.execute("UPDATE WeeklyReportTable SET totalHours = ? WHERE id = ?", (round(totalHours, 2), idPrimaryKeyToUpdate))
-                    
-                    data = self.query_table("DebugLoggingTable")
-                    finalResult = [item for item in data if str(id) in item[1]]
-                    dateToCalulate = date.isoformat(timespec="minutes")[0:10]
-                    dayOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+            isEmpty = False
+            isValid = True
+            result = self.cursor.fetchall()
+            if len(result) == 0:
+                isEmpty = True
 
-                
-                if finalResult[0][GC.LOG_MESSAGE_COLUMN_NUMBER].endswith(dateToCalulate):
-                    newInText = xComments[0][0:] + " " + dayOfWeek[date.weekday()]
-                    newOutText = xComments[1][0:] + " " + dayOfWeek[date.weekday()]
-                    print(f'Found ID {id} with {dateToCalulate} date')
-                    
-                    self.cursor.execute("UPDATE WeeklyReportTable SET inComments = ? WHERE id = ?", (newInText, idPrimaryKeyToUpdate))
-                    self.cursor.execute("UPDATE WeeklyReportTable SET outComments = ? WHERE id = ?", (newOutText, idPrimaryKeyToUpdate))
-
+      
+            if row == None and column == None:
+                return result, isEmpty, isValid
+            elif column == None:
+                return result[row-1], isEmpty, isValid
             else:
-                print("INVALID USER ID ???")
-                    
-        except ValueError:
-            self.insert_debug_logging_table(f'Invalid date format when generating weekly report on {date}')
-            
+                if column == GC.IMAGE_URL_COLUMN_NUMBER:
+                    return json.loads(result[row-1][column]), isEmpty, isValid
+                else:
+                    return result[row-1][column], isEmpty, isValid
+                  
         except IndexError:
-            print("INVALID USER ID")
-            
-        finally:
-            self.commit_changes()
+            db.insert_debug_logging_table(f'Invalid table row or column number {row} OR {column} respectively was requested')
+            return None, None, False
+        
+        except sqlite3.OperationalError:
+            db.insert_debug_logging_table(f'The {tableName} table does NOT exist in EnergyReport.db or there is typo in table name')
+            return None, None, False
 
 
     def calculate_daily_total_wattHour(self, date: datetime) -> int:
@@ -498,21 +424,17 @@ class Database:
     def is_date_between(self, startDatetimeObj, endDatetimeObj, dateToCheck) -> bool:
         return startDatetimeObj <= dateToCheck <= endDatetimeObj
 
+
 if __name__ == "__main__":
-    canUpdateweeklyReportTable = True   #TODO REMOVE AFTER Main.generate_report() and Main.update_weekly_report_table() is tested
     print("Testing Database.py")
 
     db = Database()
+    db.example_tables()
     
-    """
-    db.setup_users()
-    db.setup_weekly_report()    
-    checkInErrors = db.insert_check_in_table(1001)
-    print(checkInErrors)
-    #sleep(65)
+    insertErrors = db.insert_check_in_table(1001)
+    print(insertErrors)
     checkOutErrors = db.insert_check_out_table(1001)
-    print(checkOutErrors)
-    """
+    print(insertErrors)
     
     users = db.query_table("DailyEnergyTable")
     for data in users:
